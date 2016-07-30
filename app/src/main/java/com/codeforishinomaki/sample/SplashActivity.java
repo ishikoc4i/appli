55e1dfe18cf4b1413c2d8d99af38df885b218534
Vaironium <arudoran@gmail.com>
2016-07-30 12:00:14 +0900

    余計な文を消しました

--- app/src/main/java/com/codeforishinomaki/sample/SplashActivity.java
+++ app/src/main/java/com/codeforishinomaki/sample/SplashActivity.java
@@ -8,32 +8,16 @@ import android.os.Bundle;
 
 public class SplashActivity extends Activity {
 
-    //次は壁紙を表示するために、重ねるようにxmlファイルを編集する
-    //randomのほうも終了したら、終了するようにする
-
-    int startTime = 2000;       //メインアクティビティが起動するまでの時間
-    //int randomNumber = 8;                //ランダムでイメージを変えるやつの変数   8の場合：0～7
+    int startTime = 1000;       //メインアクティビティが起動するまでの時間
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //randomImage a_1 = new randomImage();        //randomImageクラスをインスタンス化【a_1】
-        //a_1.method();                               //メソッド呼び出し
-
-        
         Handler hdl = new Handler();
         hdl.postDelayed(new splashHandler(), startTime);    //指定した時間遅延させてsplashHandlerを実行
     }
-
-    //   class randomImage{
-//        void method(){
-//            Random rnd = new Random();         //Randomクラスのインスタンス化
-//            int ran = rnd.nextInt(randomNumber);
-//            System.out.println("random変数"+ran);
-//        }
-//    }
 
     class splashHandler implements Runnable{
         public void run(){
