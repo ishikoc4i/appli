a3b765a40bcdddabd96b98fafc6ea0537eb9e96e
Vaironium <arudoran@gmail.com>
2016-07-31 09:53:53 +0900

    スプラッシュ画面の背景をランダムで表示できるようにした

--- app/src/main/java/com/codeforishinomaki/sample/SplashActivity.java
+++ app/src/main/java/com/codeforishinomaki/sample/SplashActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class SplashActivity extends Activity {
 
@@ -15,8 +18,28 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        randomImage();                  //ランダムに背景画像を決定するやつ実行
+
         Handler hdl = new Handler();
         hdl.postDelayed(new splashHandler(), startTime);    //指定した時間遅延させてsplashHandlerを実行
+    }
+
+    void randomImage(){      //ランダムに背景画像を決定するやつ実行
+        int [] images = {
+                R.drawable.sp_pic1,
+                R.drawable.sp_pic2,
+                R.drawable.sp_pic3,
+                R.drawable.sp_pic4,
+                R.drawable.sp_pic5,
+                R.drawable.sp_pic6,
+                R.drawable.sp_pic7};
+
+        Random rnd = new Random();
+        int ran = rnd.nextInt(images.length);
+        int imageid = images[ran];
+
+        ImageView view = (ImageView)this.findViewById(R.id.image01);
+        view.setImageResource(imageid);
     }
 
     class splashHandler implements Runnable{
